b50d2ea295719abb6aacc6a9eb7289ad1511336d
Raghu Kasavajhula <rkapps.gae@gmail.com>
2026-01-07 13:22:23 -0800

    Add domain specific errors

---
+++ src/fs/errors.rs
@@ -0,0 +1,32 @@
+
+use std::path::PathBuf;
+
+use thiserror::Error;
+
+
+#[derive(Error, Debug)]
+pub enum FsDatabaseError {
+
+    #[error("Repository for collection {path} could not get created")]
+    CollectionRespositoryError { path: PathBuf },
+
+    #[error("Repository for collection {path} is missing")]
+    CollectionRepoisitoryMissingError { path: PathBuf },
+
+    #[error("Repository for collection {path} could not be downcast")]
+    CollectionRepoisitoryDowncastError { path: PathBuf },
+}
+
+#[derive(Error, Debug)]
+pub enum FsRepositoryError {
+
+    #[error("Failed to create directory: {path}")]
+    DirectoryCreation { path: PathBuf },
+
+    #[error("Failed to write file to: {path}")]
+    FileCreation { path: PathBuf },
+
+    #[error("Failed to delete file to: {path}")]
+    FileDeletion { path: PathBuf },
+
+}
