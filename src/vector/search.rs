95085ab446fc6f5790560d6a86985f3ad375c271
Raghu Kasavajhula <rkapps.gae@gmail.com>
2026-01-27 20:47:37 -0800

    add tests fo vector_search

--- src/vector/search.rs
+++ src/vector/search.rs
@@ -3,11 +3,11 @@ use crate::vector::similarity::cosine_similarity;
 // vector_search uses cosine_simularity to get the score. Returns truncated top_k.
 pub fn vector_search<K: Clone>(
     vec_a: &[f32],
-    embeddings: &[(K, Vec<f32>)],
+    candidates: &[(K, Vec<f32>)],
     top_k: usize,
 ) -> Vec<(K, f32)> {
     // Iterate the embeddings, perform cosine_similarity and return vector of id(String) and score
-    let mut scores: Vec<(K, f32)> = embeddings
+    let mut scores: Vec<(K, f32)> = candidates
         .into_iter()
         .map(|(id, vec)| (id.clone(), cosine_similarity(vec_a, vec)))
         .collect();
@@ -18,4 +18,27 @@ pub fn vector_search<K: Clone>(
     // truncate by top_k
     scores.truncate(top_k);
     scores
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::vector::search::vector_search;
+
+
+    #[test]
+    fn test_vector_search_top_k() {
+        let vec_a = vec![1.0, 0.0, 0.0];
+        let candidates = vec![
+            (1, vec![1.0, 0.0, 2.0]),
+            (2, vec![1.0, 2.0, 3.0]),
+            (3, vec![1.0, 3.0, 4.0]),
+            (4, vec![1.0, 3.0, 5.0]),
+        ];
+
+        let results = vector_search(&vec_a, &candidates, 2);
+        assert!(results.len() == 2);
+        assert_eq!(results[0].0, 1);
+        assert_eq!(results[1].0, 2);
+        println!("{:?}", results);
+    }
 }
--- src/vector/similarity.rs
+++ src/vector/similarity.rs
@@ -11,10 +11,45 @@ pub fn cosine_similarity(vec_a: &[f32], vec_b: &[f32]) -> f32 {
         return 0.0;
     }
     
-    dot_prod / (mag_a /  mag_b)
+    dot_prod / (mag_a *  mag_b)
 }
 
 // magniture - calculates the magnitude(length) of the vector
 fn magnitude(vec: &[f32]) -> f32 {
     vec.iter().map(|e| e.powf(2.0)).sum::<f32>().sqrt()
+}
+
+
+
+#[cfg(test)]
+mod tests {
+
+    use crate::vector::similarity::cosine_similarity;
+
+    #[test]
+    fn test_cosine_similarity_identical_vectors() {
+        let vec_a: Vec<f32> = vec![1.0, 2.0, 3.0];
+        let vec_b: Vec<f32> = vec![1.0, 2.0, 3.0];
+        let similarity = cosine_similarity(&vec_a, &vec_b);
+        assert!( (similarity -1.0).abs() < 0.0001 );
+    }
+
+    #[test]
+    fn test_cosine_similarity_opposite_vectors() {
+        let vec_a: Vec<f32> = vec![1.0, 2.0, 3.0];
+        let vec_b: Vec<f32> = vec![-1.0, -2.0, -3.0];
+        let similarity = cosine_similarity(&vec_a, &vec_b);
+        assert!( (similarity -1.0).abs() < 0.0001 );
+    }
+
+
+    #[test]
+    fn test_cosine_similarity_zero_vector() {
+        let vec_a = vec![1.0, 2.0, 3.0];
+        let vec_b = vec![0.0, 0.0, 0.0];
+        
+        let similarity = cosine_similarity(&vec_a, &vec_b);
+        assert_eq!(similarity, 0.0); // Should return 0.0
+    }    
+
 }
